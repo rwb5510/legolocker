cc1dd6edb9adfea60e3b80f281c74fdc2131d121
RB@H <107571028+rwb5510@users.noreply.github.com>
2025-12-03 16:39:22 -0500

    Delete web directory

--- web/assets/js/config.js
+++
@@ -1,5 +0,0 @@
-// Default configuration for local development.
-// Copy this file and update the values or rely on Docker env vars to overwrite via config.template.js.
-window.LEGOLOCKER_CONFIG = {
-  rebrickableApiKey: '',
-};
--- web/assets/js/config.template.js
+++
@@ -1,4 +0,0 @@
-// Values are injected at container start via envsubst (Docker entrypoint).
-window.LEGOLOCKER_CONFIG = {
-  rebrickableApiKey: "${REBRICKABLE_API_KEY:-}",
-};
